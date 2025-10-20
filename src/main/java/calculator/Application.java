4f51355871861b6d51b8f302067a90104a85c7c6
suho <sshsh1004@naver.com>
2025-10-20 09:38:51 +0900

    fix: Application 출력 형식 오류 수정

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -7,7 +7,11 @@ public class Application {
         System.out.println("덧셈할 문자열을 입력해 주세요.");
         String input = Console.readLine();
 
-        int result = StringCalculator.add(input);
-        System.out.println("결과 : " + result);
+        try {
+            int result = StringCalculator.add(input);
+            System.out.println("결과 : " + result);
+        } catch (IllegalArgumentException e) {
+            throw e;
+        }
     }
 }
